8de861006f809a5999144b634b3a592988878875
Zhasurbek777 <manasovzhasurbek@gmail.com>
2025-05-02 12:54:14 +0500

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import Post_list, postDetails, addPost, editPost, deletePost
 
 urlpatterns = [
-    path('', Post_list, name = 'post_list'),
+    path('', Post_list, name = 'post-list'),
     path('<int:pid>/', postDetails, name='details'),
     path('add/', addPost, name='add-new-post'),
     path('<int:pid>/edit/', editPost, name='edit-post'),
